f6a1bdf179ee232dfa8629825ecdddb3324ba1ef
lucas <lucas.belini@escola.pr.gov.br>
2024-04-17 09:50:32 -0300

    joia

---
+++ projeto2/prototipo.cs
@@ -0,0 +1,19 @@
+namespace projeto2
+
+prototipo obj = new prototipo();
+
+public partial prototipo
+{
+    public int temp{get; set;}
+
+    public string time {get; set;}
+
+    public string date{get; set;}
+
+    public int total {get; set;}
+
+    public prototipo(){
+
+    }
+    
+}
