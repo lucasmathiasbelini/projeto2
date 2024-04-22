46028545a03650538245309befd3ef48034cba01
lucas <lucas.belini@escola.pr.gov.br>
2024-04-22 09:11:26 -0300

    joia

--- projeto2/MainPage.xaml.cs
+++ projeto2/MainPage.xaml.cs
@@ -1,13 +1,37 @@
-﻿namespace projeto2;
-
-public partial class MainPage : ContentPage
+﻿namespace projeto2
 {
-	
+    public partial class MainPage : ContentPage
+    {
+        const string url = "https://api.hgbrasil.com/weather?woeid=455927&key=SuaChaveAqui"; 
+        string result;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+        public MainPage()
+        {
+            InitializeComponent();
+            atualizarTempo();
+        }
 
+        async void atualizarTempo()
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                HttpResponseMessage response = await httpClient.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    result = await response.Content.ReadAsStringAsync();
+                    
+                }
+                else
+                {
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+    }
 }
 
